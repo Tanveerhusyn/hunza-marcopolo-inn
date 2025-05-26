71b5c8532b43780c3fe37d6d7b452d87ed3237e5
tanveerhusyn <tanveerhussain465@gmail.com>
2025-05-27 03:16:26 +0500

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Hunza Marcopolo Inn",
+  description:
+    "The Hunza Marcopolo Inn is a charming hotel nestled in Gulmit, Hunza Valley, Pakistan. Positioned near the Polo Ground, it offers guests a serene environment with panoramic views of the surrounding mountains, including the iconic Passu Cones.",
+  generator: "Hunza Marcopolo Inn",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
